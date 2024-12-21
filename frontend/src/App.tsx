3360a0db4ea27eb8e533c4cf8d46969172234908
Natalya Botsman <botsmannatashaa@gmail.com>
2024-12-21 18:47:05 +0600

    Created routers and added toolbar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,21 @@
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import AllNews from './Containers/AllNews/AllNews.tsx';
+import Toolbar from './Components/UI/Toolbar/Toolbar.tsx';
+import AddNews from './Containers/AddNews/AddNews.tsx';
 
 const App = () => {
-  return <></>;
+  return <>
+    <Toolbar/>
+    <div className="container m-5">
+      <Routes>
+        <Route path="/" element={<AllNews/>}/>
+        <Route path="/news" element={<AllNews/>}/>
+        <Route path="/news/:newsId" element={<AllNews/>}/>
+        <Route path="/news/addNews" element={<AddNews/>}/>
+      </Routes>
+    </div>
+  </>;
 };
 
 export default App;
